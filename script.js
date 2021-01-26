aec367b6887738009b1432276f4af05c48ce04e2
TheRealJackiBoi <jslam@oulund.dk>
2021-01-26 12:40:41 +0100

    added welcome function

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const yourName = "Jack";
+
+const yourAge = 18;
+
+const herName = "Anna Banana";
+
+const herAge = 21;
+
+
+
+const starters = [""];
+
+const middles = [""];
+
+const ends = [""];
+
+
+const printStart = (name, herName) => {
+	console.log("Hello ", name);
+	console.log("Your message to " + herName + " will be printed soon");
+}
+
+printStart(yourName, herName);
+
+
